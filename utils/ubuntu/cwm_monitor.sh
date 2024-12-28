7f76d01fa7f0cee5bcd1437893a7ca34abc6c2c5
Ed Goodwin <ed.registration@gmail.com>
2024-12-27 21:08:26 -0600

    X11 monitor setup

---
+++ utils/ubuntu/cwm_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-1 --mode 1024x768 --pos 0x0 --rotate right --output DP-1-2 --off --output DP-1-3 --primary --mode 2560x1080 --pos 768x0 --rotate normal
---
+++ utils/ubuntu/desklayout.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+/home/egoodwin/.screenlayout/multimonitor.sh
+. /home/egoodwin/.fehbg
+pkill -USR1 polybar
