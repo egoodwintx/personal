679389d9dab0138c347b663b4c6ce6f5ddf5924a
Ed Goodwin <ed.registration@gmail.com>
2026-06-14 14:14:53 -0500

    dategap includes net working days

--- bin/dategap.py
+++ bin/dategap.py
@@ -4,6 +4,7 @@
 
 import sys
 from datetime import datetime, date
+import numpy as np
 
 date_format = '%m/%d/%Y'
 
@@ -25,15 +26,23 @@ def validateDate(arg: str) -> bool:
         return True
     
 def validatePeriodicity(arg: str) -> bool:
-    if ((arg == "days") or (arg == "weeks")):
+    if ((arg == "days") or (arg == "weeks") or (arg == "netdays")):
         return True
     else:
         return False
     
-def daysBetween(x: str, y: str) -> datetime:    
+def daysBetween(x: str, y: str) -> int:    
     if (validateDate(x) and validateDate(y)):
         daysb = datetime.strptime(y, date_format) - datetime.strptime(x, date_format)
-    return daysb
+    return daysb.days
+
+def netDaysBetween(x: str, y: str) -> int:
+    holidays = []
+    if (validateDate(x) and validateDate(y)):
+        start_date = datetime.strptime(x, date_format).date()
+        end_date = datetime.strptime(y, date_format).date()
+        daysb = np.busday_count(start_date, end_date, holidays=holidays)
+    return int(daysb)
 
 def weeksBetween(x: str, y: str) -> float:
     weeksb = daysBetween(x, y)/7
@@ -56,8 +65,20 @@ def main():
         arg3 = sys.argv[3]
         
         if(validateDate(arg1) and validateDate(arg2) and validatePeriodicity(arg3)):
-            daysint = daysBetween(arg1, arg2)            
-            print(daysint.days, "days")
+            if (arg3 == "days"):
+                daysint = daysBetween(arg1, arg2)            
+                print(daysint, "days")
+                exit(1)
+
+            elif (arg3 =="netdays"):
+                daysint = netDaysBetween(arg1, arg2)
+                print(daysint, "net working days")
+                exit(1)
+
+            else:
+                weeks = weeksBetween(arg1, arg2)
+                print(f"{weeks:.1f}", "weeks")
+                exit(1)
 
         else:
             print(defaulterror())
